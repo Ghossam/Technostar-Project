e31a800fa7e319433add3b9ebf166bdb109ff229
Ghossam <1921856.bca.cbsa@cgc.edu.in>
2023-04-03 13:12:41 +0530

    final commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>Technostar</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.youtube.com/channel/UCjXGNulmFyh6MUO3FGOVONQ" target={"blank"}>
+            Youtube
+          </a>
+          <a href="https://www.instagram.com/ghost_sam3/" target={"blank"}>
+            Instagram
+          </a>
+          <a href="https://github.com/Ghossam" target={"blank"}>
+            Github
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
